d6090eca32d7dd63d7512988f9db1abf82f36c5c
Yohann <13710295382@163.com>
2022-11-15 10:31:52 +0800

    修改输入为rtsp

--- index.js
+++ index.js
@@ -9,60 +9,61 @@ var connections = 0
 var client = new net.Socket();
 const boundaryID = "BOUNDRY";
 app.get('/', (req, res, next) => {
-    res.sendFile('./index.html', { root: __dirname });
+  res.sendFile('./index.html', { root: __dirname });
 })
 
-app.get('/vid.jpg', function(req, res) {
+app.get('/vid.jpg', function (req, res) {
 
-    res.writeHead(200, {
-        'Content-Type': 'multipart/x-mixed-replace;boundary="' + boundaryID + '"',
-        'Connection': 'keep-alive',
-        'Expires': 'Fri, 27 May 1977 00:00:00 GMT',
-        'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
-        'Pragma': 'no-cache'
-    });
+  res.writeHead(200, {
+    'Content-Type': 'multipart/x-mixed-replace;boundary="' + boundaryID + '"',
+    'Connection': 'keep-alive',
+    'Expires': 'Fri, 27 May 1977 00:00:00 GMT',
+    'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+    'Pragma': 'no-cache'
+  });
 
-    var sub = PubSub.subscribe('MJPEG', function(msg, data) {
+  var sub = PubSub.subscribe('MJPEG', function (msg, data) {
 
-        console.log(data.length);
+    console.log(data.length);
 
-        res.write('--' + boundaryID + '\r\n')
-        res.write('Content-Type: image/jpeg\r\n');
-        res.write('Content-Length: ' + data.length + '\r\n');
-        res.write("\r\n");
-        res.write(data, 'binary');
-        res.write("\r\n");
-    });
+    res.write('--' + boundaryID + '\r\n')
+    res.write('Content-Type: image/jpeg\r\n');
+    res.write('Content-Length: ' + data.length + '\r\n');
+    res.write("\r\n");
+    res.write(data, 'binary');
+    res.write("\r\n");
+  });
 
-    res.on('close', function() {
-        PubSub.unsubscribe(sub);
-        res.end();
-    });
+  res.on('close', function () {
+    PubSub.unsubscribe(sub);
+    res.end();
+  });
 });
 
 function ff() {
-    return ffmpeg('/dev/video2').videoBitrate('1024k').addInputOption("-re").outputFormat("mjpeg").fps(15).size('1080x720').addOptions("-q:v 0");
+  let rtsp = 'rtsp://admin:civicint1110@192.168.18.82:554/Streaming/Channels/101'
+  return ffmpeg(rtsp).videoBitrate('1024k').addInputOption("-re").outputFormat("mjpeg").fps(15).size('1280x720').addOptions("-q:v 0");
 }
 var command = ff();
 var ffstream = command.pipe();
-ffstream.on('data', function(chunk) {
-    if (chunk[0] == 0xFF && chunk[1] == 0xD8) {
-        PubSub.publish('MJPEG', chunks);
-        chunks = chunk;
-    } else {
-        chunks = Buffer.concat([chunks, chunk]);
-    }
+ffstream.on('data', function (chunk) {
+  if (chunk[0] == 0xFF && chunk[1] == 0xD8) {
+    PubSub.publish('MJPEG', chunks);
+    chunks = chunk;
+  } else {
+    chunks = Buffer.concat([chunks, chunk]);
+  }
 });
 
 function restart() {
-    ffstream.destroy();
-    command.kill();
-    command = ff();
-    ffstream = command.pipe();
-    ffstream.on('data', function(chunk) {
-        PubSub.publish('MJPEG', chunk);
-    });
-    ffstream.on("end", restart);
+  ffstream.destroy();
+  command.kill();
+  command = ff();
+  ffstream = command.pipe();
+  ffstream.on('data', function (chunk) {
+    PubSub.publish('MJPEG', chunk);
+  });
+  ffstream.on("end", restart);
 }
 ffstream.on("end", restart);
-app.listen(8080);
+app.listen(8666);
